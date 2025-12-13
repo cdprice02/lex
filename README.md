6d127fec29527e4b6c0ea7b509f2a0ac5090cd24
Carson Price <carsondp02@gmail.com>
2025-12-13 15:26:26 -0600

    Collect 5 letter word dataset

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # lex
+
 THE Wordle Solver
+
+## Data
+
+The main data source used for analysis was the Knuth five-letter word dictionary.
+This list contains 5678 five-letter words and their associated frequencies in from a few sampling strategies done by Don Knuth and his team.
---
+++ gram/src/main.rs
@@ -0,0 +1,42 @@
+use std::io::{BufRead, Write};
+
+fn main() {
+    const DAT_FILE_PATH: &str = "../words.dat";
+    const OUT_FILE_PATH: &str = "../words_with_frequencies.csv";
+
+    let input = std::fs::File::open(DAT_FILE_PATH).expect("file valid");
+    let input = std::io::BufReader::new(input);
+    let output = std::fs::File::create(OUT_FILE_PATH).expect("file valid");
+    let mut output = std::io::BufWriter::new(output);
+
+    for line in input.lines() {
+        let line = line.expect("failed to read line");
+        if line.starts_with('*') {
+            continue;
+        }
+
+        assert!(
+            line.len() >= 5,
+            "all lines at least have a five letter word"
+        );
+        let word = &line[0..5];
+        let freq = if line.len() == 5 {
+            0
+        } else {
+            // skip the commonness delimiter at index 5
+            let rest = if line.len() >= 6 { &line[6..] } else { "" };
+
+            if rest.is_empty() {
+                0
+            } else {
+                rest.split(',')
+                    .filter(|p| !p.is_empty())
+                    .map(|p| p.parse::<u64>().expect("frequency is a number"))
+                    .sum()
+            }
+        };
+
+        let output_line = format!("{},{}\n", word, freq);
+        output.write_all(output_line.as_bytes()).unwrap();
+    }
+}
