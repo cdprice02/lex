a14ede6ce372d73f40835a141aca5f8d3dd3ac38
cdprice02 <cdprice02@gmail.com>
2026-05-28 09:15:00 -0500

    cli for configuration

--- lex/src/main.rs
+++ lex/src/main.rs
@@ -1,3 +1,5 @@
+use clap::Parser;
+
 mod data;
 mod game;
 mod guesser;
@@ -6,8 +8,35 @@ mod word;
 use data::load_wordle_words;
 use game::play;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(
+        short = 'l',
+        long,
+        default_value = "5",
+        help = "Length of the words to use in the game"
+    )]
+    word_length: usize,
+    #[arg(
+        short = 'n',
+        long,
+        default_value = "0",
+        help = "Number of games to simulate (0 for all)"
+    )]
+    num_games: usize,
+}
+
 fn main() {
-    let wordle_words = load_wordle_words::<5>();
+    let args = Args::parse();
+
+    // TODO: move gram project to module within lex
+    let wordle_words = load_wordle_words::<5>(); // TODO: use args.word_length
+    let wordle_words = if args.num_games == 0 {
+        wordle_words
+    } else {
+        wordle_words.into_iter().take(args.num_games).collect()
+    };
     let num_words = wordle_words.len();
 
     let results = wordle_words.into_iter().map(|word| {
