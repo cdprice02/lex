36c79506d9fdf204dbe97a8be81834926656cd80
cdprice02 <cdprice02@gmail.com>
2026-06-05 16:28:49 -0500

    fix word set getter to appropriately use num_games and dictionary_length
    before the dictionary was truncated based on the number of games which caused non-deterministic behavior based on the number of games. this fix truncates the lists appropriately and warns the user that games might repeat words if the dictionary is not large enough to support the number of games to simulate.

--- lex/src/lib.rs
+++ lex/src/lib.rs
@@ -36,29 +36,33 @@ macro_rules! configure_word_length_bounds {
 configure_word_length_bounds!(3, 10);
 
 pub fn run<const N: usize>(args: &Args) -> anyhow::Result<()> {
-    // TODO: add word selection strategies (e.g. random, most/least frequent, etc.) instead of just taking the first n words
-    let word_set = lex_data::blocking::get::<N>(&args.data_dir, args.lang, args.num_games)?;
-    let words = word_set.words();
-    let num_words = words.len();
+    let word_set = lex_data::blocking::get::<N>(&args.data_dir, args.lang, args.dictionary_length)?;
+    let num_games = args.num_games.unwrap_or(word_set.len());
+    let dictionary_length = word_set.len();
+    if dictionary_length < num_games {
+        log::warn!(
+            "dictionary length ({dictionary_length}) is less than number of games ({num_games}), so some words may be repeated in the games"
+        );
+    }
 
     log::info!(
         "Simulating {} games with {}-letter words in {}...",
-        num_words,
+        num_games,
         N,
         args.lang
     );
 
     let mut results = Vec::new();
-    for &word in words {
+    for &word in word_set.words().iter().cycle().take(num_games) {
         let result = play(word, &word_set)?;
         log::debug!("{}: {}", result.word(), result.num_guesses());
         results.push(result);
     }
 
-    log::info!("Completed {} games", num_words);
+    log::info!("Completed {} games", num_games);
 
     let avg_guesses =
-        results.iter().map(|r| r.num_guesses() as f64).sum::<f64>() / num_words as f64;
+        results.iter().map(|r| r.num_guesses() as f64).sum::<f64>() / num_games as f64;
     log::info!("Average number of guesses: {avg_guesses:.2}");
 
     Ok(())
